14bc10372aab54e9eeb74a44cf736edf11a2e25c
Sidney Maestre <sid.maestre@gmail.com>
2021-10-07 09:43:42 -0700

    Change the reqestBody from being inline to a component/requestBodies
    That component is defined as allOf two component/schema.  When Java code is generated, the two schema are combined into the PetEditable class.

--- generator/build.sh
+++ generator/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./scripts/pet-java.sh
-./scripts/pet-docs.sh
+#./scripts/pet-docs.sh
--- generator/scripts/pet-java.sh
+++ generator/scripts/pet-java.sh
@@ -4,15 +4,19 @@ SCRIPT="$0"
 echo "# START SCRIPT: $SCRIPT"
 
 # ---- Docker Run ---
-args2="generate 
+args1="generate 
 -t /local/modules/Java 
 -g java 
--i https://raw.githubusercontent.com/SidneyAllen/asc-2021-rebel/main/petstore.yaml
+-i /myapp/petstore.yaml
 --library google-api-client 
 -c /local/scripts/java.json  
 -o /local/output/java 
 --model-package com.pet.models"
 #--global-property debugModels,debugOperations
+#-i https://raw.githubusercontent.com/SidneyAllen/asc-2021-rebel/main/petstore.yaml
+
 
 echo "**************  Generate Pet Java SDK ******************"
-docker run --rm -v ${PWD}:/local -e JAVA_OPTS="-Dlog.level=error" openapitools/openapi-generator-cli:v4.3.1 $args2
+#docker run --rm -v ${PWD}:/local -e JAVA_OPTS="-Dlog.level=error" openapitools/openapi-generator-cli:v4.3.1 $args1
+
+docker run --rm -v ${PWD}:/local --mount type=bind,source="$(pwd)"/../,target=/myapp  -e JAVA_OPTS="-Dlog.level=error" openapitools/openapi-generator-cli:v5.2.1 $args1
